d0eac0a376eb5524a30ffbcf22f60fa6bdf513c7
tomoya0220-gz <tomoya02201998@gmail.com>
2024-06-17 18:30:46 +0900

    サーバーを実装

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -16,6 +17,12 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
